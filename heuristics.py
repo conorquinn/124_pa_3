dfb12d6c6e8b6626d9fbdb7ed76fbcea410559f7
Seon Joong Kim <seonjoongkim@college.harvard.edu>
2017-04-12 21:25:15 -0400

    done with standard form

--- heuristics.py
+++ heuristics.py
@@ -1,12 +1,12 @@
 import random
 max_iter = 25000
- 
+n = 100
 # HELPER FUNCTIONS
     
 # Generate a random solution
 def gen_rand_sol():
   rand_lst = []
-  for i in range (0, 100):
+  for i in range (0, n):
     sign = random.randint(0,1)
     if sign == 0: 
       sign -= 1
@@ -28,7 +28,7 @@ def residue(S, A):
 # Generate a random list A for testing
 def rand_lst():
   lst = []
-  for i in range(0,100):
+  for i in range(0,n):
     lst.append(random.randint(0, 10000))
   return lst
   
@@ -36,10 +36,47 @@ def rand_lst():
 def repeated_random():
   A = rand_lst()
   S = gen_rand_sol()
-  for i in range(1, max_iter):
+  for i in range(0, max_iter):
     _S = gen_rand_sol()
     if (residue(_S, A) < residue(S, A)):
       S = _S
   return S
   
-print repeated_random();
+# Generates a random neighbor of S 
+def rand_neighbor(S):
+  i = 0
+  j = 0
+  while (i == j):
+      i = random.randint(0, 99)
+      j = random.randint(0, 99)
+  pr_swap = random.randint(0,1)
+  S[i] = S[i] * -1
+  if pr_swap == 1:
+    S[j] = S[j] * -1
+  return S
+  
+# Hill Climbing 
+def hill_climbing():
+  A = rand_lst()
+  S = gen_rand_sol()
+  for i in range(0, max_iter):
+    _S = rand_neighbor(S)
+    if (residue(_S, A) < residue(S, A)):
+      S = _S
+  return S
+
+# Simulated Annealing
+def simulated_annealing():
+  A = rand_lst()
+  S = gen_rand_sol()
+  best = S
+  for i in range(0, max_iter):
+    _S = rand_neighbor(S)
+    if residue(_S, A) < residue(S, A):
+      S = _S
+    if residue(S, A) < residue(best, A):
+      best = S
+  return best 
+  
+
+
