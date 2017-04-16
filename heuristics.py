a36aef8997f0e5de153f274d4b95c86800151cdc
Seon Joong Kim <seonjoongkim@college.harvard.edu>
2017-04-16 17:35:35 -0400

    done w standard form

--- heuristics.py
+++ heuristics.py
@@ -1,6 +1,25 @@
 import random
+import math
+from heap import MaxHeap
+
 max_iter = 25000
 n = 100
+largest = 10 ** 12
+
+def kkarp(A):
+  maxh = MaxHeap()
+  for n in A:
+      maxh.heappush(n)
+  n1 = maxh.heappop()
+  n2 = maxh.heappop()
+  while n2 > 0:
+    diff = n1 - n2
+    maxh.heappush(diff)
+    maxh.heappush(0)
+    n1 = maxh.heappop()
+    n2 = maxh.heappop()
+  return n1
+
 # HELPER FUNCTIONS
     
 # Generate a random solution
@@ -29,7 +48,7 @@ def residue(S, A):
 def rand_lst():
   lst = []
   for i in range(0,n):
-    lst.append(random.randint(0, 10000))
+    lst.append(random.randint(0, largest))
   return lst
   
 # Repeated random
@@ -65,6 +84,9 @@ def hill_climbing():
       S = _S
   return S
 
+def T(i):
+  return tenten*0.8*(math.floor(i/300))
+
 # Simulated Annealing
 def simulated_annealing():
   A = rand_lst()
@@ -74,9 +96,16 @@ def simulated_annealing():
     _S = rand_neighbor(S)
     if residue(_S, A) < residue(S, A):
       S = _S
+    else:
+      diff = -1 *(residue(_S,A) - residue (S,A))
+      if diff > 0:
+        n = math.exp(diff/T(i))
+        p = random.uniform(0,1)
+        if n >= p:
+          S = _S
     if residue(S, A) < residue(best, A):
       best = S
   return best 
-  
 
+print simulated_annealing()
 
